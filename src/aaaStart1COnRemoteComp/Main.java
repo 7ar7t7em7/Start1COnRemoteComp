b1711267fd8c9748ae1eb8d0e02a247b1bc0513f
7ar7t7em7 <ar7t7em7@yandex.ru>
2026-09-23 14:04:16 +0300

    starting commit

---
+++ src/aaaStart1COnRemoteComp/Main.java
@@ -0,0 +1,9 @@
+package aaaStart1COnRemoteComp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module aaaStart1COnRemoteComp {
+}
